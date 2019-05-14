5142fdf43d426b717a6015efc6aed26196056f79
Shim Heungwoon <ajaxlab7@gmail.com>
2019-05-14 22:28:00 +0900

    Extend draggable area

--- src/clock.js
+++ src/clock.js
@@ -1,11 +1,9 @@
-const {ipcRenderer} = require('electron');
+const { ipcRenderer } = require('electron');
 
 function startClock() {
 
-    const W = 150;
-    const H = 150;
-    const CW = W / 2 + 0.5;
-    const CH = H / 2 + 0.5;
+    const W = 108;
+    const H = 108;
     const R = 50;
     const R_RULE_IN = 45;
     const R_RULE_OUT = 50;
@@ -14,25 +12,37 @@ function startClock() {
     const RMINUTES = 40;
     const RSECONDS = 40;
     const FRAME_COLOR = '#fff';
- 
+
     const canvas = window.document.getElementById('c');
     const ctx = canvas.getContext('2d');
-    
+
+    let canvasSizeRatio = 1;
     let staticData = null;
     let isDrag = false;
     const startPos = {
         x: null,
         y: null
     };
- 
+
     function addCanvasListeners() {
+        function sendMoveEnd() {
+            isDrag = false;
+            setCanvasRatio(1);
+            tick();
+            ipcRenderer.send('moveEnd');
+        }
         canvas.addEventListener('mousedown', (e) => {
+            const bounds0 = getCanvasBounds();
+            setCanvasRatio(8);
+            const bounds = getCanvasBounds();
+            tick();
+            ipcRenderer.send('moveStart');
             isDrag = true;
-            startPos.x = e.clientX;
-            startPos.y = e.clientY;
+            startPos.x = e.clientX + (bounds.width - bounds0.width) / 2;
+            startPos.y = e.clientY + (bounds.height - bounds0.height) / 2;
         });
         canvas.addEventListener('mouseup', () => {
-            isDrag = false;
+            sendMoveEnd();
         });
         canvas.addEventListener('mousemove', (e) => {
             const delta = {
@@ -40,98 +50,115 @@ function startClock() {
                 y: e.clientY - startPos.y
             };
             if (isDrag) {
-                ipcRenderer.send('moveDelta', delta);
+                ipcRenderer.send('move', delta);
+            }
+        });
+        canvas.addEventListener('mouseleave', (e) => {
+            if (isDrag) {
+                sendMoveEnd();
             }
         });
     }
 
     function clear() {
-        ctx.clearRect(0, 0, W, H);
+        const bounds = getCanvasBounds();
+        ctx.clearRect(0, 0, bounds.width, bounds.height);
     }
- 
-    function getCircumferencePoint(ratio, radius) {
-        const theta = Math.PI * 2 * ratio;
+
+    function getCanvasBounds() {
+        const height = H * canvasSizeRatio;
+        const width = W * canvasSizeRatio;
         return {
-            x: CW + radius * Math.sin(theta),
-            y: CH - radius * Math.cos(theta)
+            height,
+            width,
+            centerH: height / 2 + 0.5,
+            centerW: width / 2 + 0.5
         };
     }
- 
+
+    function getCircumferencePoint(ratio, radius) {
+        const theta = Math.PI * 2 * ratio;
+        const bounds = getCanvasBounds();
+        return {
+            x: bounds.centerW + radius * Math.sin(theta),
+            y: bounds.centerH - radius * Math.cos(theta)
+        };
+    }
+
     function init() {
-        canvas.width = W;
-        canvas.height = H;
-        renderFrame();
-        // renderIndices();
-        renderRules();
-        staticData = ctx.getImageData(0, 0, W, H);
+        setCanvasRatio(1);
         addCanvasListeners();
         tick();
     }
- 
+
     function renderBg() {
         ctx.putImageData(staticData, 0, 0);
     }
 
     function renderCenterPoint() {
+        const { centerW, centerH } = getCanvasBounds();
         ctx.beginPath();
-        ctx.arc(CW, CH, 5, 0, 2 * Math.PI);
+        ctx.arc(centerW, centerH, 5, 0, 2 * Math.PI);
         ctx.fill();
     }
- 
+
     function renderFrame() {
+        const { centerW, centerH } = getCanvasBounds();
         ctx.save();
         ctx.lineWidth = 4;
         ctx.fillStyle = FRAME_COLOR;
         ctx.beginPath();
-        ctx.arc(CW, CH, R, 0, 2 * Math.PI);
+        ctx.arc(centerW, centerH, R, 0, 2 * Math.PI);
         ctx.stroke();
         ctx.fill();
         ctx.restore();
     }
- 
+
     function renderIndices() {
         ctx.save();
         ctx.fillStyle = '#fff';
         ctx.font = '10px arial bold';
         for (let i = 1; i < 13; i++) {
-            const p = getCircumferencePoint( i / 12, RINDICES);
+            const p = getCircumferencePoint(i / 12, RINDICES);
             const measure = ctx.measureText(i);
-            const halfWidth =  measure.width / 2;
+            const halfWidth = measure.width / 2;
             ctx.fillText(i, p.x - halfWidth, p.y + halfWidth);
         }
         ctx.restore();
     }
- 
+
     function renderHourHand(hour) {
+        const { centerW, centerH } = getCanvasBounds();
         const p = getCircumferencePoint(hour / 12, RHOURS);
         ctx.save();
         ctx.lineWidth = 4;
         ctx.lineCap = 'round';
         ctx.strokeStyle = '#333';
         ctx.beginPath();
-        ctx.moveTo(CW, CH);
+        ctx.moveTo(centerW, centerH);
         ctx.lineTo(p.x, p.y);
         ctx.stroke();
         ctx.restore();
     }
- 
+
     function renderMinuteHand(minute) {
+        const { centerW, centerH } = getCanvasBounds();
         const p = getCircumferencePoint(minute / 60, RMINUTES);
         ctx.save();
         ctx.lineWidth = 2;
         ctx.lineCap = 'round';
         ctx.beginPath();
-        ctx.moveTo(CW, CH);
+        ctx.moveTo(centerW, centerH);
         ctx.lineTo(p.x, p.y);
         ctx.stroke();
         ctx.restore();
     }
- 
+
     function renderRules() {
         ctx.save();
         for (let i = 1; i < 61; i++) {
-            const pIn = getCircumferencePoint( i / 60, R_RULE_IN);
-            const pOut = getCircumferencePoint( i / 60, R_RULE_OUT);
+            const pIn = getCircumferencePoint(i / 60, R_RULE_IN);
+            const pOut = getCircumferencePoint(i / 60, R_RULE_OUT);
             if (i % 5) {
                 ctx.lineWidth = 1;
             } else {
@@ -144,18 +171,27 @@ function startClock() {
         }
         ctx.restore();
     }
- 
+
     function renderSecondHand(second) {
+        const { centerW, centerH } = getCanvasBounds();
         const p = getCircumferencePoint(second / 60, RSECONDS);
         ctx.save();
         ctx.strokeStyle = 'red';
         ctx.beginPath();
-        ctx.moveTo(CW, CH);
+        ctx.moveTo(centerW, centerH);
         ctx.lineTo(p.x, p.y);
         ctx.stroke();
         ctx.restore();
     }
- 
+
+    function setCanvasRatio(ratio) {
+        canvasSizeRatio = ratio || 1;
+        const { width, height } = getCanvasBounds();
+        canvas.width = width;
+        canvas.height = height;
+        updateCache();
+    }
+
     function tick() {
         const d = new Date();
         const msec = d.getMilliseconds();
@@ -170,12 +206,19 @@ function startClock() {
         renderCenterPoint();
         // requestAnimationFrame(tick);
     }
- 
+
+    function updateCache() {
+        renderFrame();
+        // renderIndices();
+        renderRules();
+        const { width, height } = getCanvasBounds();
+        staticData = ctx.getImageData(0, 0, width, height);
+    }
+
     init();
- 
+
     // tick();
     setInterval(tick, 1000);
- }
- 
- startClock();
- 
+}
+
+startClock();
--- src/createWindow.js
+++ src/createWindow.js
@@ -1,14 +1,20 @@
 const { BrowserWindow, ipcMain } = require('electron');
 
 let win;
+const W = 108;
+const H = 108;
 
 function createWindow() {
     win = new BrowserWindow({
+        webPreferences: {
+            nodeIntegration: true,
+        },
         alwaysOnTop: true,
         frame: false,
         transparent: true,
-        width: 300,
-        height: 300,
+        //    resizable: false,
+        width: W,
+        height: H,
         skipTaskbar: true
     });
     win.loadURL(`file://${__dirname}/index.html`);
@@ -18,9 +24,38 @@ function createWindow() {
     });
 }
 
-ipcMain.on('moveDelta', (event, delta) => {
+function getWindowBounds(windowRatio) {
+    const pos = win.getPosition();
+    const size = win.getSize();
+    const x0 = pos[0];
+    const y0 = pos[1];
+    const w0 = size[0];
+    const h0 = size[1];
+    const width = w0 * windowRatio;
+    const height = h0 * windowRatio;
+    const x = x0 - (width - w0) / 2;
+    const y = y0 - (height - h0) / 2;
+    return {x, y, width, height};
+}
+
+function extendWindow(ratio) {
+    const {x, y, width, height} = getWindowBounds(ratio);
+    console.log('extendWindow', x, y, width, height);
+    win.setPosition(x, y);
+    win.setSize(width, height);
+}
+
+ipcMain.on('moveStart', (event) => {
+    extendWindow(8);
+});
+
+ipcMain.on('move', (event, delta) => {
     const p = win.getPosition();
     win.setPosition(p[0] + delta.x, p[1] + delta.y);
+});
+
+ipcMain.on('moveEnd', (event) => {
+    extendWindow(1/8);
 });
 
 module.exports = createWindow;
